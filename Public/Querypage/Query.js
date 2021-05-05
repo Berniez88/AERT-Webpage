05e781dd7d3f59040681a54dd9ac354ecca7f3d0
bzhong88 <berniezhong@gmail.com>
2021-05-05 16:56:07 -0400

    query page comments

--- Public/Querypage/Query.js
+++ Public/Querypage/Query.js
@@ -1,100 +1,4 @@
-// // This example adds a user-editable rectangle to the map.
-// // When the user changes the bounds of the rectangle,
-// // an info window pops up displaying the new bounds.
-// let rectangle;
-// let map;
-// let infoWindow;
-
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: 37.09, lng: -95.71 },
-//     zoom: 9,
-//   });
-//   const bounds = {
-//     north: 38.12,
-//     south:37.0267,
-//     east: -94.739,
-//     west: -96.882,
-//   };
-//   // Define the rectangle and set its editable property to true.
-//   rectangle = new google.maps.Rectangle({
-//     bounds: bounds,
-//     editable: true,
-//     draggable: true,
-//   });
-//   rectangle.setMap(map);
-//   // Add an event listener on the rectangle.
-//   rectangle.addListener("bounds_changed", showNewRect);
-//   // Define an info window on the map.
-//   infoWindow = new google.maps.InfoWindow();
-// }
-
-// /** Show the new coordinates for the rectangle in an info window. */
-// function showNewRect() {
-//   const ne = rectangle.getBounds().getNorthEast();
-//   const sw = rectangle.getBounds().getSouthWest();
-//   const contentString =
-//     "<b>Rectangle moved.</b><br>" +
-//     "New north-east corner: " +
-//     ne.lat() +
-//     ", " +
-//     ne.lng() +
-//     "<br>" +
-//     "New south-west corner: " +
-//     sw.lat() +
-//     ", " +
-//     sw.lng();
-//   // Set the info window's content and position.
-//   infoWindow.setContent(contentString);
-//   infoWindow.setPosition(ne);
-//   infoWindow.open(map);
-// }
-
-
-// The following example creates complex markers to indicate beaches near
-// Sydney, NSW, Australia. Note that the anchor is set to (0,32) to correspond
-// to the base of the flagpole.
-
-// const minLong = document.querySelector('.minLong');
-// const mLat = document.querySelector('.minLat');
-// const maxLong = document.querySelector('.maxLong');
-// const minLat = document.querySelector('.maxLat');
-// function initMap() {
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//       zoom: 10,
-//       center: { lat: 0, lng: 0 },
-//     });
-//     setMarkers(map);
-//   }
-//   // Data for the markers consisting of a name, a LatLng and a zIndex for the
-//   // order in which these markers should display on top of each other.
-//   const beaches = [
-//       ["Longitude", ]
-//     ["Bondi Beach", -33.890542, 151.274856, 4],
-//     ["Coogee Beach", -33.923036, 151.259052, 5],
-//     ["Cronulla Beach", -34.028249, 151.157507, 3],
-//     ["Manly Beach", -33.80010128657071, 151.28747820854187, 2],
-//     ["Maroubra Beach", -33.950198, 151.259302, 1],
-//   ];
-//   const beaches = [
-    
-//   ]
-  
-//   function setMarkers(map) {
-//     var bounds = new google.maps.LatLngBounds();
-//     // Adds markers to the map.
-//     for (let i = 0; i < beaches.length; i++) {
-//       const beach = beaches[i];
-//       var marker = new google.maps.Marker({
-//         position: { lat: beach[1], lng: beach[2] },
-//         map,
-//         title: beach[0],
-//       });
-//       bounds.extend(marker.getPosition());
-//     }
-//     map.fitBounds(bounds);
-//   }
-
+// Multiple lists that store each specific data in the user input for the query page
 const minLong = [];
 const minLat = [];
 const maxLong = [];
@@ -145,7 +49,7 @@ function display(){
   endinghour.value = "";
   xemail.value = "";
 
-    // Show our output
+    // Show our output on the left side, this is just a visual representation to make sure the code functions
     // messageBox.innerHTML = "";
   
     // messageBox.innerHTML += "Minimum Longitude: " + minLong.join(", ") + "<br/>";
@@ -159,6 +63,8 @@ function display(){
     // messageBox.innerHTML += "Email: " + email.join(", ");
 
 }
+
+// This function below allows the user to click reset in order to clear all the data in the table, the commented out code will just set the date to the most recent date
 function setDateAndHour() {
   // var date = new Date();
   // var day = date.getDate();
@@ -188,6 +94,8 @@ function setDateAndHour() {
   document.getElementById('maxlat').value = '';
   document.getElementById('email').value = '';
 }
+
+// This function generates the entire google map API 
 
 function initMap(){
   var options ={
